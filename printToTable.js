fb7c892153c9d0f3f3c24fd22f0ba14973c52924
Kristian Barrett <kbarrett-86@hotmail.com>
2014-03-02 14:08:01 +0100

    Made it even smaller and with less iterations

--- printToTable.js
+++ printToTable.js
@@ -1,35 +1,23 @@
 /*
- * Generates table rows and populates them with the information from the object passed to the function
- * and adds the rows to the table body of the table identified by the ID.
+ * Generates a table row and populates it with the information from the object passed to the function.
+ * After that adds the row to the table body of the table identified by the ID.
  */
 
 ;function printObjectInTable(objectToIterate, tableID) {
-    var propertyOrder = getTHeadIDs(tableID),
-        numberOfProperties = propertyOrder.length,
+    var thChildren = document.getElementById(tableID).getElementsByTagName("th"),
+        childrenLength = thChildren.length,
+        numberOfProperties = thChildren.length,
         tr = document.createElement("tr");
-
-    for (var prop in propertyOrder) {
+    console.log(thChildren);
+    for (var i = 0; i < thChildren.length; i++) {
+        var th = thChildren[i];
         // important check that this is objects own property 
         // not from prototype prop inherited
         var td = document.createElement("td");
-        if (objectToIterate.hasOwnProperty(prop)) {
-            td.appendChild(document.createTextNode(objectToIterate[prop]));
+        if (objectToIterate.hasOwnProperty(th.id)) {
+            td.appendChild(document.createTextNode(objectToIterate[th.id]));
         }
         tr.appendChild(td);
     }
     document.getElementById(tableID).getElementsByTagName("tbody")[0].appendChild(tr);
-}
-
-/*
- * Generates an array of the table header IDs. Used to know where to put the properties of the object.
- * returns - the array of table header IDs.
- */
-function getTHeadIDs(tableID){
-    var thChildren = document.getElementById(tableID).getElementsByTagName("th"),
-        childrenLength = thChildren.length,
-        tableHeadIDs = []
-    for(var i = 0; i < childrenLength; i++){
-        tableHeadIDs[i] = thChildren[i].id;
-    }   
-    return tableHeadIDs;
 }
